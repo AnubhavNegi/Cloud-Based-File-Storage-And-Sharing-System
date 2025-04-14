412a748b19e93ffe29a50eace7052447c97c339b
AnubhavNegi <anubhavnegi5@gmail.com>
2025-04-14 18:48:52 +0530

    First Commit| Project Setup| Breeze Installation| DB Migrations| Models Creation

---
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Navigation from "@/Components/Navigation.vue"
+</script>
+
+<template>
+    <div class="h-screen bg-gray-50 flex w-full gap-4">
+        <Navigation/>
+    </div>
+</template>
