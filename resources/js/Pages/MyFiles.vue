3071036fa3080e58b95c3f2199e05230d3786347
AnubhavNegi <anubhavnegi5@gmail.com>
2025-04-26 15:31:28 +0530

    Added path attribute while creating folders

--- resources/js/Pages/MyFiles.vue
+++ resources/js/Pages/MyFiles.vue
@@ -1,9 +1,35 @@
 <template>
     <AuthenticatedLayout>
-        {{ $page.url }}
+        <table class="min-w-full">
+          <thead class="bg-gray-100 border-b">
+            <tr>
+              <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Name
+              </th>
+              <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Owner
+              </th>
+              <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Last Modified
+              </th>
+              <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Size
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            
+          </tbody>
+        </table>
     </AuthenticatedLayout>
 </template>
 
 <script setup>
+  //Imports
   import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+  //Props & Emit
+  const {files} = defineProps({
+    files: Object
+  })
 </script>
