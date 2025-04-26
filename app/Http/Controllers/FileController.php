73adddb8c86f3bcbe7284043974d5bd46f5c17de
AnubhavNegi <anubhavnegi5@gmail.com>
2025-04-26 23:30:54 +0530

    Partially implemented File Upload functionality

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\File;
 use App\Http\Resources\FileResource;
+use App\Http\Requests\StoreFileRequest;
 use App\Http\Requests\StoreFolderRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Query\Builder;
@@ -54,6 +55,13 @@ class FileController extends Controller
         $file->name = $data['name'];
 
         $parent->appendNode($file);
+    }
+
+    public function store(StoreFileRequest $request)
+    {
+        $data = $request->validated();
+
+        dd($data);
     }
 
     private function getRoot()
--- resources/js/Components/app/CreateNewDropdown.vue
+++ resources/js/Components/app/CreateNewDropdown.vue
@@ -3,8 +3,7 @@
         
         <MenuButton
         class="flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold
-        text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-        >
+        text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           Create New
         </MenuButton>
         
@@ -28,16 +27,8 @@
               </MenuItem>
             </div>
             <div class="px-1 py-1">
-              <MenuItem v-slot="{ active }">
-                <a class="text-gray-700 block px-4 py-2 text-sm">
-                    Upload Files
-                </a>
-              </MenuItem>
-              <MenuItem v-slot="{ active }">
-                <a class="text-gray-700 block px-4 py-2 text-sm">
-                    Upload Folder
-                </a>
-              </MenuItem>
+              <FileUploadMenuItem />
+              <FolderUploadMenuItem />
             </div>
           </MenuItems>
         </transition>
@@ -50,6 +41,8 @@
     import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
     import CreateFolderModal from "@/Components/app/CreateFolderModal.vue";
     import {ref} from "vue";
+    import FileUploadMenuItem from '@/Components/app/FileUploadMenuItem.vue';
+    import FolderUploadMenuItem from '@/Components/app/FolderUploadMenuItem.vue';
 
     // Refs
     const createFolderModal = ref(false)
